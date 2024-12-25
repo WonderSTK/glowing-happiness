385af9c240fec8ba3c96eaff65cd231ca77edb47
WonderSTK <mehulparmar9694@gmail.com>
2024-12-25 17:08:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  
+
+  return (
+    <div className="flex flex-row items-center justify-center h-screen bg-gray-100 font-sans text-lg">  
+      <Faq />
+      </div>
+  )
+}
+
+export default App
